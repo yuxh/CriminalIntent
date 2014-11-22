0cfe67af9d37a8798e9af3bb62d7eb3229d280d8
yuxh <yuxh00@gmail.com>
2014-11-22 14:19:09 +0800

    update layout

--- myAndroid/CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
+++ myAndroid/CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,15 +1,36 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
+
+	private boolean mSolved;
 
 	public Crime() {
 		// Generate unique identifier
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	public String getTitle() {
