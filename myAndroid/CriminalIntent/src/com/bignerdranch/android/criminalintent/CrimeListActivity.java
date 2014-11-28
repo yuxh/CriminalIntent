d3b366a9045464c6d507d4cbee3d46760fa03bfa
yuxh00 <yuxh00@gmail.com>
2014-11-28 21:32:17 +0800

    refactor to template pattern

--- myAndroid/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ myAndroid/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,12 +2,11 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
 	@Override
 	protected Fragment createFragment() {
-		// TODO Auto-generated method stub
-		return new CrimeFragment();
+		return new CrimeListFragment();
 	}
 
 }
